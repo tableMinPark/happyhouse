a0f2c8ffd814e78af2038e288bd1c696a99e05e5
tablemin_park <tablemin_park@daum.net>
2022-11-14 22:53:49 +0900

    FE: DealPage Layout 완료

--- frontend/src/components/view/DealPage.vue
+++ frontend/src/components/view/DealPage.vue
@@ -11,11 +11,9 @@
           <div id="search-wrapper" class="row">      
               <div class="container-fruid"> 
                 <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0"> 
-                  <div class="info-block">
-                    <h6><feather type="search" size="15" class="pe-2"/>검색종류를 선택하세요</h6>
-                  </div>
+                  <div class="info-block"><h6><feather type="search" size="15" class="pe-2"/>검색종류를 선택하세요</h6></div>
                 </div>    
-                <div class="card shadow shadow-showcase p-3 text-center">
+                <div class="card shadow shadow-showcase p-3 mb-1 text-center">
                   <div class="container-fluid">
                     <!-- 검색 종류 선택 START -->
                     <div class="row pb-2">
@@ -29,7 +27,6 @@
                         </div>
                     </div>
                     <!-- 검색 종류 선택 END -->
-
                     <!-- 동, 키워드 검색 START -->
                     <div v-show="searchType == 'A'" class="row">
                         <div class="col-10 pe-0 d-flex">
@@ -40,7 +37,6 @@
                         </div>
                     </div>
                     <!-- 동, 키워드 검색 END --> 
-
                     <!-- 주소 검색 START -->
                     <div v-show="searchType == 'K'" class="row">
                         <div class="col-10 pe-0 d-flex">
@@ -62,151 +58,137 @@
                 </div>
               </div>
             </div>
-              <div v-if="houseInfo != null" class="container-fruid"> 
-                <div class="card shadow shadow-showcase mb-2">
-                  <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
-                    <div class="info-block">
+            <div class="default-according" id="accordion">
+              <div class="card">
+                <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-2"  id="headingFour"> 
+                  <div class="info-block">
+                    <button @click="dealInfoToggler" class="btn btn-link text-white" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">  
                       <h6>검색된 매물 (2)</h6>
-                    </div>
-                  </div>                    
-                  <div class="container card-body"> 
-                    <div class="row">
-                      <h3>삼정그린코아</h3>
-                      <p>부산시 강서구 송정동 1627-5</p>
-                      <div class="col-4">
-                        <h6>건축년도</h6>
-                      </div>
-                      <div class="col-8">
-                        <h6>2022</h6>                          
-                      </div>
-                    </div>
+                    </button>
                   </div>
-                </div>
-
-                <div class="card shadow shadow-showcase mb-2">
-                  <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
-                    <div class="info-block">
-                      <h6>검색된 매물 (2)</h6>
-                    </div>
-                  </div>                    
-                  <div class="container card-body"> 
-                    <div class="row">
-                      <h3>삼정그린코아</h3>
-                      <p>부산시 강서구 송정동 1627-5</p>
-                      <div class="col-4">
-                        <h6>건축년도</h6>
-                      </div>
-                      <div class="col-8">
-                        <h6>2022</h6>                          
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="card shadow shadow-showcase mb-2">
-                  <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
-                    <div class="info-block">
-                      <h6>리뷰</h6>
-                    </div>
-                  </div>                    
-                  <div class="container-fruid card-body"> 
-                    <div class="row">
-                      <div class="col-8">   
-                        <div class="media">
-                          <img class="img-50 img-fluid m-r-20 rounded-circle" alt src="assets/images/user/2.png">
-                          <div class="media-body">
-                            <span class="d-block">박 상 민</span>
-                            <span class="d-block">2022.11.11 가입</span>
+                </div> 
+                <div class="collapse" :class="{show: dealInfoToggle}" id="collapseFour" aria-labelledby="headingOne" data-bs-parent="#accordion">
+                  <div class="container-fruid"> 
+                    <div class="card shadow shadow-showcase mb-2">                
+                      <div class="container card-body"> 
+                        <div class="row">
+                          <h3>삼정그린코아</h3>
+                          <p>부산시 강서구 송정동 1627-5</p>
+                          <div class="col-4">
+                            <h6>건축년도</h6>
+                          </div>
+                          <div class="col-8">
+                            <h6>2022</h6>                          
                           </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-
-                <div class="card shadow shadow-showcase mb-2">
-                  <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
-                    <div class="info-block">
-                      <h6>실거래가</h6>
-                    </div>
-                  </div>                    
-                  <div class="container-fruid card-body p-0 pb-2"> 
-                    <div class="row">
-                      <div class="col-12">   
-                        <div class="col-sm-12 col-lg-12 col-xl-12">
-                          <div class="table-responsive">
-                            <table class="table">
-                              <colgroup>
-                                <col span="1" style="width: 20%" />
-                                <col span="1" style="width: 40%" />
-                                <col span="1" style="width: 20%" />
-                                <col span="1" style="width: 20%" />
-                              </colgroup>
-                              <thead class="table-primary">
-                                <tr>
-                                  <th scope="col">거래일</th>
-                                  <th scope="col">거래가격</th>
-                                  <th scope="col">면적</th>
-                                  <th scope="col">층수</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td>1</td>
-                                  <td>2</td>
-                                  <td>3</td>
-                                  <td>4</td>
-                                </tr>
-                              </tbody>
-                            </table>
+                    <div class="card shadow shadow-showcase mb-2">
+                      <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
+                        <div class="info-block">
+                          <h6>리뷰</h6>
+                        </div>
+                      </div>                    
+                      <div class="container-fruid card-body"> 
+                        <div class="row">
+                          <div class="col-8">   
+                            <div class="media">
+                              <img class="img-50 img-fluid m-r-20 rounded-circle" alt src="assets/images/user/2.png">
+                              <div class="media-body">
+                                <span class="d-block">박 상 민</span>
+                                <span class="d-block">2022.11.11 가입</span>
+                              </div>
+                            </div>
                           </div>
-                        </div>             
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div class="card shadow shadow-showcase mb-2">
-                  <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
-                    <div class="info-block">
-                      <h6>매물</h6>
-                    </div>
-                  </div>                    
-                  <div class="container-fruid card-body p-0 pb-2"> 
-                    <div class="row">
-                      <div class="col-12">   
-                        <div class="col-sm-12 col-lg-12 col-xl-12">
-                          <div class="table-responsive">
-                            <table class="table">
-                              <colgroup>
-                                <col span="1" style="width: 20%" />
-                                <col span="1" style="width: 40%" />
-                                <col span="1" style="width: 20%" />
-                                <col span="1" style="width: 20%" />
-                              </colgroup>
-                              <thead class="table-primary">
-                                <tr>
-                                  <th scope="col">거래일</th>
-                                  <th scope="col">거래가격</th>
-                                  <th scope="col">면적</th>
-                                  <th scope="col">층수</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td>1</td>
-                                  <td>2</td>
-                                  <td>3</td>
-                                  <td>4</td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>             
+                    <div class="card shadow shadow-showcase mb-2">
+                      <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
+                        <div class="info-block">
+                          <h6>실거래가</h6>
+                        </div>
+                      </div>                    
+                    <div class="container-fruid card-body p-0 pb-2"> 
+                      <div class="row">
+                        <div class="col-12">   
+                          <div class="col-sm-12 col-lg-12 col-xl-12">
+                            <div class="table-responsive">
+                              <table class="table">
+                                <colgroup>
+                                  <col span="1" style="width: 20%" />
+                                  <col span="1" style="width: 40%" />
+                                  <col span="1" style="width: 20%" />
+                                  <col span="1" style="width: 20%" />
+                                </colgroup>
+                                <thead class="table-primary">
+                                  <tr>
+                                    <th scope="col">거래일</th>
+                                    <th scope="col">거래가격</th>
+                                    <th scope="col">면적</th>
+                                    <th scope="col">층수</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    <td>4</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                          </div>             
+                        </div>
                       </div>
                     </div>
-                  </div>
+                    </div>
+                    <div class="card shadow shadow-showcase mb-2">
+                      <div class="card-header bg-primary d-flex justify-content-center align-items-center pb-0 pt-2"> 
+                        <div class="info-block">
+                          <h6>매물</h6>
+                        </div>
+                      </div>                    
+                      <div class="container-fruid card-body p-0 pb-2"> 
+                        <div class="row">
+                          <div class="col-12">   
+                            <div class="col-sm-12 col-lg-12 col-xl-12">
+                              <div class="table-responsive">
+                                <table class="table">
+                                  <colgroup>
+                                    <col span="1" style="width: 20%" />
+                                    <col span="1" style="width: 40%" />
+                                    <col span="1" style="width: 20%" />
+                                    <col span="1" style="width: 20%" />
+                                  </colgroup>
+                                  <thead class="table-primary">
+                                    <tr>
+                                      <th scope="col">거래일</th>
+                                      <th scope="col">거래가격</th>
+                                      <th scope="col">면적</th>
+                                      <th scope="col">층수</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <td>1</td>
+                                      <td>2</td>
+                                      <td>3</td>
+                                      <td>4</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </div>             
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>   
                 </div>
-            </div>  
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -226,18 +208,26 @@ export default {
       gugun: '군',
       dong: '동',
       keyword: '',
-      houseInfo: null
+      houseInfo: "1",
+
+      dealInfoToggle: false
     }
   },
   components: {
     BasicHeader
   },
   methods: {
+    dealInfoToggler() {
+      if (this.dealInfoToggle) this.dealInfoToggle = false;
+      else this.dealInfoToggle = true;
+    },
     addressSearch() {
       console.log("call address search " + this.si + " " + this.gugun + " " + this.dong);
+      this.dealInfoToggle = true;
     },  
     keywordSearch() {
       console.log("call keyword search " + this.keyword);
+      this.dealInfoToggle = true;
     },  
     initMap() {
       let mapContainer = document.querySelector("#kakao-map");
@@ -284,18 +274,18 @@ export default {
     width: 400px;
     height: 100%;
     overflow-x: hidden;
-    padding: 10px;
+    padding: 10px 5px;
     position: absolute;
 }
 
-#search-wrapper {
-}
-
-#search-result-wrapper {
+#accordion > .card,
+#collapseFour {
+  background-color:transparent;
+  border-color:transparent;
 }
 
 .card {
-  background-color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 0.8);
 }
 
 
