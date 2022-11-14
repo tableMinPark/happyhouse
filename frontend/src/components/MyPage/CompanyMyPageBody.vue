9c51c4dbb435f5df1931bc427c5d67018e57f3c5
tablemin_park <tablemin_park@daum.net>
2022-11-15 03:20:08 +0900

    FE: Profile 수정

--- frontend/src/components/MyPage/CompanyMyPageBody.vue
+++ frontend/src/components/MyPage/CompanyMyPageBody.vue
@@ -24,7 +24,7 @@
       </div>
       
       <div class="row">
-        <div class="col-sm-12 col-xl-6 xl-100">
+        <div class="col-sm-12">
             <div class="card">
                 <div class="card-body">
                 <ul class="nav nav-tabs border-tab" id="top-tab" role="tablist">
